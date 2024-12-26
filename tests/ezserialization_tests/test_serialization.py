822d3f9e01c9a3b3a5d5d4f3f74d5ac504402873
Matas Gumbinas <matas.gumbinas@gmail.com>
2024-12-26 16:44:44 +0200

    feat: Implement ezserialization & add tests

---
+++ tests/ezserialization_tests/test_serialization.py
@@ -0,0 +1,55 @@
+from typing import Mapping, cast
+
+from ezserialization import Serializable, deserialize, serializable
+
+
+@serializable  # <- valid for serialization
+@serializable(name="A")
+@serializable(name="XXX")
+class _CaseAUsingAutoName(Serializable):
+    def __init__(self, value: str):
+        self.value = value
+
+    def to_dict(self) -> Mapping:
+        return {"value": self.value}
+
+    @classmethod
+    def from_dict(cls, src: Mapping):
+        return cls(value=src["value"])
+
+    @classmethod
+    def abs_qualname(cls) -> str:
+        return f"{cls.__module__}.{cls.__qualname__}"
+
+
+@serializable(name="B")  # <- valid for serialization
+@serializable(name="YYY")
+@serializable
+@serializable(name="ZZZ")
+class _CaseBUsingNameAlias(Serializable):
+    def __init__(self, value: str):
+        self.value = value
+
+    def to_dict(self) -> Mapping:
+        return {"value": self.value}
+
+    @classmethod
+    def from_dict(cls, src: Mapping):
+        return cls(value=src["value"])
+
+
+def test_serialization_typenames_order():
+    """
+    Expected behaviour: Only the top typename is used to serialize instances.
+    On the other hand, for deserialization all typenames are valid.
+    """
+
+    a = _CaseAUsingAutoName("a")
+    data = a.to_dict()
+    assert data["_type_"] == _CaseAUsingAutoName.abs_qualname()
+    assert a.value == cast(_CaseAUsingAutoName, deserialize(data)).value
+
+    b = _CaseBUsingNameAlias("b")
+    data = b.to_dict()
+    assert data["_type_"] == "B"
+    assert b.value == cast(_CaseBUsingNameAlias, deserialize(data)).value
